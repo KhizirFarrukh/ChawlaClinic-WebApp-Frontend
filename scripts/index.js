83fc87b7b3b7e66d76e5fe9283a686dec0cc6c3b
Khizir Farrukh <khizir.farrukh@cipherimpact.com>
2024-03-07 12:27:01 +0500

    homepage ready

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,14 @@ function disableCaseNoInput() {
     document.getElementById('caseno').placeholder = "23B-012345";
   }
 }
+
+function clearSelection(name) {
+  var radioButtons = document.getElementsByName(name);
+  radioButtons.forEach(function (radio) {
+    radio.checked = false;
+  });
+}
+
+function disabledFunctionality(){
+  alert('This functionality is disabled right now.');
+}
