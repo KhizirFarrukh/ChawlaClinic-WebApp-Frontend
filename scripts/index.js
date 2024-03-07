9c7d4cc4135cbc135a0f812aad558c09671c3d72
Khizir Farrukh <khizir.farrukh@cipherimpact.com>
2024-03-07 15:46:46 +0500

    added loading icon to search

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 function disableCaseNoInput() {
-  if (document.getElementById('caseno').disabled) {
-    document.getElementById('caseno').disabled = false;
-    document.getElementById('caseno').value = "";
-    document.getElementById('caseno').placeholder= "1234";
+  if (document.getElementById("caseno").disabled) {
+    document.getElementById("caseno").disabled = false;
+    document.getElementById("caseno").value = "";
+    document.getElementById("caseno").placeholder = "1234";
   } else {
-    document.getElementById('caseno').disabled = true;
-    document.getElementById('caseno').value = "";
-    document.getElementById('caseno').placeholder = "23B-012345";
+    document.getElementById("caseno").disabled = true;
+    document.getElementById("caseno").value = "";
+    document.getElementById("caseno").placeholder = "23B-012345";
   }
 }
 
@@ -17,59 +17,75 @@ function clearSelection(name) {
   });
 }
 
-function disabledFunctionality(){
-  alert('This functionality is disabled right now.');
+function disabledFunctionality() {
+  alert("This functionality is disabled right now.");
 }
 
 function cleanObject(obj) {
   for (var propName in obj) {
-      if (obj[propName] === null || obj[propName] === undefined) {
-          delete obj[propName];
-      }
+    if (obj[propName] === null || obj[propName] === undefined) {
+      delete obj[propName];
+    }
   }
   return obj;
 }
 
 $(document).ready(function () {
-  $('#search-patient').on('click', function () {
+  $("#search-patient").on("click", function () {
+    $("#search-patient")
+      .prop("disabled", true)
+      .contents()
+      .filter(function () {
+        return this.nodeType === 3;
+      })
+      .remove();
+
+    $("#search-loading").removeClass("d-none");
+
     $.ajax({
-      url: '/config.json', 
-      type: 'GET',
-      dataType: 'json',
+      url: "/config.json",
+      type: "GET",
+      dataType: "json",
       success: function (configData) {
-        console.log('API URL:', configData.apiUrl);
+        console.log("API URL:", configData.apiUrl);
 
         var requestData = {
-          SearchParam: $('#searchParam').val(),
+          SearchParam: $("#searchParam").val(),
           Type: $('input[name="patient-type"]:checked').val() ?? null,
           Status: $('input[name="patient-status"]:checked').val() ?? null,
-          FirstVisitStart: $('#first-visit-start').val() ?? null,
-          FirstVisitEnd: $('#first-visit-end').val() ?? null,
+          FirstVisitStart: $("#first-visit-start").val() ?? null,
+          FirstVisitEnd: $("#first-visit-end").val() ?? null,
           Size: 10,
           Page: 1,
           IsAscending: true,
-          SortColumn: null
+          SortColumn: null,
         };
 
         requestData = cleanObject(requestData);
 
         var queryString = $.param(requestData);
-        
+
         $.ajax({
           url: configData.apiUrl + "?" + queryString,
-          type: 'GET',
-          dataType: 'json',
+          type: "GET",
+          dataType: "json",
           success: function (apiResponse) {
-            console.log('API Response:', apiResponse);
+            console.log("API Response:", apiResponse);
           },
           error: function (error) {
-            console.error('Error:', error);
-          }
+            console.error("Error:", error);
+          },
         });
+
+        $("#search-patient").prop("disabled", false).append("Search");
+        $("#search-loading").addClass("d-none");
       },
       error: function (error) {
-        console.error('Error:', error);
-      }
+        console.error("Error:", error);
+
+        $("#search-patient").prop("disabled", false).append("Search");
+        $("#search-loading").addClass("d-none");
+      },
     });
   });
 });
